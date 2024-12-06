6a44770d20ded31dc631b20676b480ff9d0b8f8c
egmonpereira <egmonpereira@gmail.com>
2024-12-06 12:09:01 -0300

    Reiniciando o projeto do zero

--- venv/simplemooc/core/models.py
+++
@@ -1,6 +0,0 @@
-#MODELS: modelos dos bancos de dados, é a persistência dos nossos dados
-#TEMPLATES: é a visualização, como os dados serão visualizados
-
-from django.db import models
-
-# Create your models here.
--- venv/simplemooc/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, re_path
-from core.views import home, contact
-
-app_name = 'core'
-urlpatterns = [
-    path("", home, name='home'),
-    re_path(r'^contact.html/$', contact, name='contact'),
-]
