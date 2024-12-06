a36d37299dd9d55d15f108bccb89192a84808575
egmonpereira <egmonpereira@gmail.com>
2024-12-06 17:52:15 -0300

    Conclusão da Seção 1: Introdução e Conceitos básicos

---
+++ venv/simplemooc/core/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from core import views
+
+urlpatterns = [
+    path('', views.home, name=''),
+    path('index/', views.home, name='inicio'),
+    path('contato/', views.contact, name='contato'),
+]
---
+++ venv/simplemooc/core/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    #return HttpResponse('Hello Word!')
+    #return render(request, 'home.html', {'usuario': 'Fulano de tal'})
+    return render(request, 'home.html')
+
+def contact(request):
+    return render(request, 'contact.html')
